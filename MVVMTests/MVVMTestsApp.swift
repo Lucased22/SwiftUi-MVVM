dd62e445fe7e054405181c133279a039c9d145dc
Lucased22 <lucaseduardo.siq@gmail.com>
2022-12-12 14:45:55 -0400

    Initial Commit

---
+++ MVVMTests/MVVMTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTestsApp.swift
+//  MVVMTests
+//
+//  Created by Lucas Santos on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
